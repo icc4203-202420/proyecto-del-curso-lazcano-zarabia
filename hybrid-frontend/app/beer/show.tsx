7ed46a63df6343261e07b4efc3a9e94489ad5d27
Dlazcano8 <dlazcano1@miuandes.cl>
2024-10-15 00:01:08 -0300

    beer own info

--- hybrid-frontend/app/beer/show.tsx
+++ hybrid-frontend/app/beer/show.tsx
@@ -1,15 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
 
+interface Beer {
+  id: number;
+  name: string;
+  style: string;
+  alcohol: string;
+  // Puedes agregar más propiedades según sea necesario
+}
 
 export default function BeerShow() {
   const { id } = useLocalSearchParams(); 
+  const [beer, setBeer] = useState<Beer | null>(null); // beer es un objeto que puede ser null inicialmente
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchBeerDetails = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:3001/api/v1/beers/${id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch beer details');
+        }
+        const result = await response.json();
+        console.log("Datos recibidos del backend:", result);
+        setBeer(result.beer); // Aquí almacenamos el objeto beer directamente
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching beer details:', error);
+        setIsError(true);
+        setIsLoading(false);
+      }
+    };
+
+    fetchBeerDetails();
+  }, [id]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Cargando detalles de la cerveza...</Text>
+      </View>
+    );
+  }
+
+  if (isError) {
+    return (
+      <View style={styles.container}>
+        <Text>Error al cargar los detalles de la cerveza.</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Detalles de la Cerveza</Text>
-      <Text>ID de la Cerveza: {id}</Text>
+
+      {beer && (
+        <>
+          <Text style={styles.title}>Detalles de la Cerveza {beer.name}</Text>
+          <Text>Estilo: {beer.style}</Text>
+          <Text>Alcohol: {beer.alcohol} </Text>
+        </>
+      )}
     </View>
   );
 }
